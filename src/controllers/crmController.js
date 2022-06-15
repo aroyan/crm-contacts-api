a9fee94c63f9de8ba74d26463e2dc22dd6ffdcf6
Aroyan Bakti Setiawan <abssneakers@gmail.com>
2022-06-15 18:47:10 +0800

    feat: add prettier config file and fix search query using regex

--- src/controllers/crmController.js
+++ src/controllers/crmController.js
@@ -75,14 +75,17 @@ export const deleteContact = (req, res) => {
 
 export const searchContact = (req, res) => {
   const { q, page = 0, limit = 5 } = req.query;
-  Contact.find({ firstName: q }, (err, contact) => {
-    if (err) {
-      res.send(err);
+  Contact.find(
+    { firstName: { $regex: new RegExp(q, "i") } },
+    (err, contact) => {
+      if (err) {
+        res.send(err);
+      }
+      if (!contact.length) {
+        res.json({ error: `${q} not found` });
+      } else {
+        res.json({ total: contact.length, contact });
+      }
     }
-    if (!contact.length) {
-      res.json({ error: `${q} not found` });
-    } else {
-      res.json({ total: contact.length, contact });
-    }
-  });
+  );
 };
